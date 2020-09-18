a562390758e3325faffcf70b52ebe9e8d61d634f
Gisela Acosta <gacosta@technisys.com>
2020-09-18 08:04:35 -0300

    primer commit

---
+++ relevamiento/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC6GOsOKzqSq-8FSwqC3qGOxCeky8ksHPM",
+    authDomain: "relevamiento-cf61d.firebaseapp.com",
+    databaseURL: "https://relevamiento-cf61d.firebaseio.com",
+    projectId: "relevamiento-cf61d",
+    storageBucket: "relevamiento-cf61d.appspot.com",
+    messagingSenderId: "975504295855",
+    appId: "1:975504295855:web:ec215450fc40f6c5a4115a",
+    measurementId: "G-PQS6F9YLFG"
+  }
+};
---
+++ relevamiento/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+    firebaseConfig : {
+    apiKey: "AIzaSyC6GOsOKzqSq-8FSwqC3qGOxCeky8ksHPM",
+    authDomain: "relevamiento-cf61d.firebaseapp.com",
+    databaseURL: "https://relevamiento-cf61d.firebaseio.com",
+    projectId: "relevamiento-cf61d",
+    storageBucket: "relevamiento-cf61d.appspot.com",
+    messagingSenderId: "975504295855",
+    appId: "1:975504295855:web:ec215450fc40f6c5a4115a",
+    measurementId: "G-PQS6F9YLFG"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
