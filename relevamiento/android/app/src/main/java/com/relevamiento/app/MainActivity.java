c40bb30f18e31cf55b0a76ba08ba678c487a9889
giselaacosta <gi_selita2007@hotmail.com>
2021-04-11 16:38:56 -0300

    cambio

--- relevamiento/android/app/src/main/java/com/relevamiento/app/MainActivity.java
+++ relevamiento/android/app/src/main/java/com/relevamiento/app/MainActivity.java
@@ -1,57 +1,21 @@
 package com.relevamiento.app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ActivityOptions;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Pair;
-import android.view.View;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+import java.util.ArrayList;
 
-        // Agregar animaciones
-        //Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
-        Animation animacion = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        //TextView deTextView = findViewById(R.id.deTextView);
-        //final TextView codeliaTextView = findViewById(R.id.codeliaTextView);
-        final ImageView logoImageView = findViewById(R.id.imageView4);
-
-        //deTextView.setAnimation(animacion);
-        //codeliaTextView.setAnimation(animacion2);
-        logoImageView.setAnimation(animacion);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-
-                Pair[] pairs = new Pair[1];
-                pairs[0] = new Pair<View, String>(logoImageView, "logoImageTrans");
-               // pairs[1] = new Pair<View, String>(codeliaTextView, "textTrans");
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
-                    startActivity(intent, options.toBundle());
-                } else {
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        }, 4000);
-    }
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins you've installed go here
+      // Ex: add(TotallyAwesomePlugin.class);
+    }});
+  }
 }
